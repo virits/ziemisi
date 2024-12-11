1ea9378adc9a69b93e8fffe34f6432eba2c8b7c4
virits <roberts_virsis@o1vsk.lv>
2024-12-11 10:29:59 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+const button = document.querySelector('button')
+
+button.addEventListener('mouseover', () => {
+    window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley')
+})
